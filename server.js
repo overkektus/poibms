61544dc6f9bea44e4d6b989d1f4e3d9af8b26564
Egor Piskunou2 <egor.piskunou@gmail.com>
2018-03-23 20:49:39 +0300

    basic https server

---
+++ server.js
@@ -0,0 +1,20 @@
+const https = require('https');
+const pem = require('pem');
+const express = require('express');
+ 
+pem.config({
+  pathOpenSSL: 'C:/OpenSSL-Win32/bin/openssl.exe'
+})
+
+pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+  if (err) {
+    throw err
+  }
+  const app = express();
+ 
+  app.get('/', function (req, res) {
+    res.send('o hai!')
+  });
+ 
+  https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(4300)
+})
